f03cc7c3b7962a3b125385fed61f096e176b21f5
EggRoll <JyEggRoll@outlook.com>
2026-02-17 10:50:10 +0800

    ♻️ refac: fix 时用户体验升级

--- cmd/fix.go
+++ cmd/fix.go
@@ -78,7 +78,7 @@ func RunFix(cmd *cobra.Command, args []string) {
 
 	// 交互循环
 	for {
-		input, err := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("输入要修复的编号（空格分隔），'all' 或 'a' 修复所有，'d<编号>' 删除条目，如 d7，单次只能删除一个，'exit' 或 'e' 退出")
+		input, err := pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("输入 all 或 a 修复所有\n输入 d<编号> 删除条目，如 d7，单次只能删除一个\n输入 exit 或 e 退出程序\n输入数字以修复对应项，使用空格分隔\n")
 		if err != nil {
 			logger.Error("输入错误 " + err.Error())
 			continue
