787f3fe7dd3aa225bd9f276383387fd36b0e2b60
EggRoll <JyEggRoll@outlook.com>
2026-02-28 19:44:32 +0800

    ✨ feat: 版本号自动注入

--- cmd/version.go
+++ cmd/version.go
@@ -6,16 +6,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Version 应用版本号
-const Version = "0.0.7"
+// 由 GitHub 构建时自动注入
+var Version = "dev"
 
-// versionCmd represents the version command
+// 由 GitHub 构建时自动注入
+var BuildTime = "unknown"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "显示 flk 版本信息",
-	Long:  "显示 flk 工具的版本信息",
+	Short: "显示版本信息",
+	Long:  "显示版本信息",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("flk 的当前版本为 %s\n", Version)
+		fmt.Printf("版本：%s\n构建时间：%s\n", Version, BuildTime)
 	},
 }
 
